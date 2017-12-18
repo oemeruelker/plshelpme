9ded399dfd361813974d83ea7e8759c21cafa4e1
Ömer Ülker <omerulker@Omers-MacBook-Pro.local>
2017-12-18 17:39:38 +0100

    the problem

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -5,7 +5,7 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="animated fadeIn">
-        Hello World
+        <img src="https://i.imgur.com/7JeWMTm.png"/>
       </div>
     )
   }
